ebebb239e054d722ee59988ff610b35b6ce6e7c9
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-22 12:20:42 +0300

    create items table and add constraints

--- sql/schema_albums.sql
+++ sql/schema_albums.sql
@@ -11,3 +11,20 @@ CREATE TABLE genres (
   name varchar(45),
   PRIMARY KEY(id)
 );
+
+CREATE TABLE items (
+  id int GENERATED ALWAYS AS IDENTITY(MINVALUE 0 START WITH 0 CACHE 20) NOT NULL,
+  PRIMARY KEY (id),
+  genre varchar(30),
+  author varchar(30),
+  source varchar(30),
+  label varchar(30),
+  publish_date int,
+  archived boolean,
+  author_id int,
+  game_id int,
+  album_id int, 
+  genre_id int,
+  CONSTRAINT kf_albums FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE,
+  CONSTRAINT kf_genres FOREIGN KEY(genre_id) REFERENCES genres(id) ON DELETE CASCADE
+); 
