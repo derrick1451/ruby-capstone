eeeb3c1a5bff329149a5d87ae29d6976e83f2368
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-22 16:19:02 +0300

    Add properties and construnctor of item class

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,14 @@
 require 'date'
 
 class Item
+    attr_accessor :id, :genre, :author, :label, :publish_date, :archived
+
+    def initialize(params)
+      @id = params[:id] || rand(1..10_000)
+      @genre = params[:genre]
+      @author = params[:author]
+      @label = params[:label]
+      @publish_date = create_date(params[:publish_date])
+      @archived = params[:archived] || false
+    end
 end
