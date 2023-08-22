c544c28b7001a9df52c8a13fd7fcb2a6e09e3ef9
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-22 13:37:21 +0300

    Add music album class

--- lib/music/music_album.rb
+++ lib/music/music_album.rb
@@ -1,7 +1,7 @@
 class MusicAlbum
-    attr_accessor :on_spotify
-  
-    def initiliaze(on_spotify)
-      @on_spotify = on_spotify
-    end
+  attr_accessor :on_spotify
+
+  def initiliaze(on_spotify)
+    @on_spotify = on_spotify
+  end
 end
