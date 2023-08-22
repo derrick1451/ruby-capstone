97a002c063d996ba310d6473a117a2284ce2e743
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-22 16:28:29 +0300

    Associate music album class with item class

--- lib/music/music_album.rb
+++ lib/music/music_album.rb
@@ -1,7 +1,16 @@
-class MusicAlbum
+require_relative 'item'
+
+class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initiliaze(on_spotify)
-    @on_spotify = on_spotify
+  def initialize(params)
+    super(params)
+    @on_spotify = params[:on_spotify]
+  end
+
+  private
+
+  def can_be_archived?
+    super() && @on_spotify
   end
 end
