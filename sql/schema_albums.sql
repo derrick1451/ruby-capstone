d9bec10bf794a944b8202442194f75ab93944b4f
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-22 12:17:09 +0300

    create albums table

---
+++ sql/schema_albums.sql
@@ -0,0 +1,7 @@
+CREATE TABLE albums (
+  id int GENERATED ALWAYS AS IDENTITY,
+  on_spotify boolean,
+  PRIMARY KEY(id),
+  item_id int NOT NULL,
+  CONSTRAINT kf_items FOREIGN KEY(item_id) REFERENCES items(id) ON DELETE CASCADE
+);
