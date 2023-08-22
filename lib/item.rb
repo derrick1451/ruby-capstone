52aa074cba20150d825cac3aac9c6626035e6bef
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-22 16:21:29 +0300

    Add_genere method

--- lib/item.rb
+++ lib/item.rb
@@ -1,18 +1,22 @@
 require 'date'
 
 class Item
-    attr_accessor :id, :genre, :author, :label, :publish_date, :archived
+  attr_accessor :id, :genre, :author, :label, :publish_date, :archived
 
-    def initialize(params)
-      @id = params[:id] || rand(1..10_000)
-      @genre = params[:genre]
-      @author = params[:author]
-      @label = params[:label]
-      @publish_date = create_date(params[:publish_date])
-      @archived = params[:archived] || false
-    end
-    def move_to_archive
-        @archived = true if can_be_archived?
-      end
-    
+  def initialize(params)
+    @id = params[:id] || rand(1..10_000)
+    @genre = params[:genre]
+    @author = params[:author]
+    @label = params[:label]
+    @publish_date = create_date(params[:publish_date])
+    @archived = params[:archived] || false
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  def add_genre(genre)
+    @genre = genre
+  end
 end
