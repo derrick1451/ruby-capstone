d12cb6f5203967047d92f26d51e5ef4200b85bf3
Derrick1451 <haylederrick14@gmail.com>
2023-08-22 14:17:23 +0300

    Update linters

--- book.rb
+++ book.rb
@@ -1,10 +1,10 @@
-class Book < Item
+class Book
   def initialize(publiser, cover_state)
     @publiser = publiser
     @cover_state = cover_state
   end
 
   def can_be_archived?
-    super || @cover_state == 'bad'
+    @cover_state == 'bad'
   end
 end
