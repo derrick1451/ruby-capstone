4e1848a2ce24ce0fb89d7db0bb4ddabf7273bb71
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-23 15:01:13 +0300

    initialize variables

--- lib/helpers/collection_handler.rb
+++ lib/helpers/collection_handler.rb
@@ -3,5 +3,11 @@ require_relative './preserve_data'
 module CollectionHandler
   include PreserveData
   attr_reader :albums, :genres, :labels, :items
-
+  def instantiate_common_variables
+    @albums = []
+    @genres = []
+    @labels = []
+    @items = {}
+    @authors = []
+  end
 end
