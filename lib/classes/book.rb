53a4531c6684f1e3613da299e9824bcfad764073
Derrick1451 <haylederrick14@gmail.com>
2023-08-23 13:19:24 +0300

    Update class files

---
+++ lib/classes/book.rb
@@ -0,0 +1,13 @@
+require_relative 'item'
+class Book < Item
+  attr_accessor :publisher, :cover_state
+  def initialize(publiser, cover_state)
+    super(publish_date)
+    @publiser = publiser
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
+end
