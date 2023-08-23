9235dfbf5b1f112078d3e49599e51e4272385885
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-23 15:02:00 +0300

    define select item for method

--- lib/helpers/collection_handler.rb
+++ lib/helpers/collection_handler.rb
@@ -10,4 +10,35 @@ module CollectionHandler
     @items = {}
     @authors = []
   end
+
+  private
+
+  def select_item_for(class_name)
+    options = list_available_items
+
+    if options.empty?
+      puts 'You need to add a Item to the collection first'
+      return false
+    end
+
+    display_items_options(options, class_name)
+
+    print 'Select your Option: '
+    option_id = gets.chomp.to_i
+
+    items = options.values[option_id]
+
+    items.each_with_index do |item, i|
+      puts display_items_of_class(item, i)
+    end
+
+    print "\nEnter the Item Option: "
+    item_index = gets.chomp.to_i
+
+    if item_index.abs > items.length
+      puts "We don't have that Item, try again.\n\n"
+      return false
+    end
+    items[item_index]
+  end
 end
