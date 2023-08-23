b7348563882e9881453278fcb3d7a38b47c84ab5
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-23 15:25:02 +0300

    define list all genres

--- lib/classes/album_collection.rb
+++ lib/classes/album_collection.rb
@@ -5,6 +5,21 @@ require_relative '../helpers/collection_handler'
 
 module AlbumCollection
   include CollectionHandler
+  def list_all_albums(albums: @albums)
+    return if check_empty(albums, 'Albums', '8')
 
+    albums.each_with_index do |album, idx|
+      puts "[#{idx}] - #{album}"
+    end
+    puts ''
+  end
+  def list_all_genres(genres: @genres)
+    return if check_empty(genres, 'Genres', '11')
+
+    genres.each_with_index do |genre, idx|
+      puts "[#{idx}] - #{genre}"
+    end
+    puts ''
+  end
   
 end
