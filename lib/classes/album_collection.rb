455a0cb3bcc9799d0a705c500ec4028f1b43d16d
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-23 15:15:12 +0300

    create album collection module

---
+++ lib/classes/album_collection.rb
@@ -0,0 +1,10 @@
+require_relative './music_album'
+require_relative './genre'
+require_relative '.item'
+require_relative '../helpers/collection_handler'
+
+module AlbumCollection
+  include CollectionHandler
+
+  
+end
