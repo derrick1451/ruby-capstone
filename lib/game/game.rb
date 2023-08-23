c4c4dd3f50c3fef6b498a28bdd8d8cbaeb069efb
kayjee <kagiso.killagram@gmail.com>
2023-08-23 02:46:10 +0200

    fixed lint errors

--- lib/game/game.rb
+++ lib/game/game.rb
@@ -1,12 +1,12 @@
 class Game
-    attr_accessor :multiplayer, :last_played_at
-  
-    def initialize(multiplayer: false, last_played_at: nil)
-      @multiplayer = multiplayer
-      @last_played_at = last_played_at
-    end
-  
-    def play
-      # Do something to play the game.
-    end
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(multiplayer: false, last_played_at: nil)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
   end
+
+  def play
+    # Do something to play the game.
+  end
+end
