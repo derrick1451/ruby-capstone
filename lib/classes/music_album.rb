32550f1cf80824ea1650d7b4094a953b312abade
Derrick1451 <haylederrick14@gmail.com>
2023-08-24 11:49:47 +0300

    Edit music album file

--- lib/classes/music_album.rb
+++ lib/classes/music_album.rb
@@ -9,11 +9,11 @@ class MusicAlbum < Item
   end
 
   def to_json(*_args)
-    JSON.dump({
-                on_spotify: @on_spotify,
-                published_date: @published_date,
-                archived: @archived
-              })
+    JSON.dump(
+      on_spotify: @on_spotify,
+      published_date: @published_date,
+      archived: @archived
+    )
   end
 
   def to_s
