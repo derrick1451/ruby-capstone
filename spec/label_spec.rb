5814b65dbe03dcaa3040598d70119e0d411cfb68
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-24 16:19:39 +0300

    Add test cases for author class

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -24,53 +24,53 @@ describe Label do
       expect(book.label).to eq @label1
     end
     it 'Label.add_item(MA: MA) => MusicAlbum.label = self' do
-        music_album = MusicAlbum.new(true, '2021-12-02')
-        @label1.add_item(music_album)
-        expect(@label1.items).to include(music_album)
-        expect(music_album.label).to eq @label1
-      end
-      it 'Label.add_item(Game: game) => Game.label = self' do
-        game = Game.new('Zelda', false, 2, '2021-12-02')
-        @label1.add_item(game)
-        expect(@label1.items).to include(game)
-        expect(game.label).to eq @label1
-      end
+      music_album = MusicAlbum.new(true, '2021-12-02')
+      @label1.add_item(music_album)
+      expect(@label1.items).to include(music_album)
+      expect(music_album.label).to eq @label1
     end
-    context 'Label Serialization' do
-      it 'Label.json => JSON object with iv preserved' do
-        expected_obj = JSON.dump(
-          title: @label2.title,
-          color: @label2.color,
-          items: @label2.items.map(&:to_json)
-        )
-        expect(@label2.to_json).to eq expected_obj
-      end
-      it 'Label.json => JSON object with iv preserved Items != empty' do
-        expected_obj = JSON.dump(
-          title: @label1.title,
-          color: @label1.color,
-          items: @label1.items.map(&:to_json)
-        )
-        expect(@label1.to_json).to eq expected_obj
-      end
+    it 'Label.add_item(Game: game) => Game.label = self' do
+      game = Game.new('Zelda', false, 2, '2021-12-02')
+      @label1.add_item(game)
+      expect(@label1.items).to include(game)
+      expect(game.label).to eq @label1
     end
-    context 'Label deserialization' do
-      it 'Label.from_json => JSON object with same Title and Color variables' do
-        expected_obj1 = JSON.dump(
-          title: @label1.title,
-          color: @label1.color,
-          items: @label1.items.map(&:to_json)
-        )
-        expected_obj2 = JSON.parse(JSON.dump(
-            title: @label2.title,
-            color: @label2.color,
-            items: @label2.items.map(&:to_json)
-          ))
+  end
+  context 'Label Serialization' do
+    it 'Label.json => JSON object with iv preserved' do
+      expected_obj = JSON.dump(
+        title: @label2.title,
+        color: @label2.color,
+        items: @label2.items.map(&:to_json)
+      )
+      expect(@label2.to_json).to eq expected_obj
+    end
+    it 'Label.json => JSON object with iv preserved Items != empty' do
+      expected_obj = JSON.dump(
+        title: @label1.title,
+        color: @label1.color,
+        items: @label1.items.map(&:to_json)
+      )
+      expect(@label1.to_json).to eq expected_obj
+    end
+  end
+  context 'Label deserialization' do
+    it 'Label.from_json => JSON object with same Title and Color variables' do
+      expected_obj1 = JSON.dump(
+        title: @label1.title,
+        color: @label1.color,
+        items: @label1.items.map(&:to_json)
+      )
+      expected_obj2 = JSON.parse(JSON.dump(
+                                   title: @label2.title,
+                                   color: @label2.color,
+                                   items: @label2.items.map(&:to_json)
+                                 ))
 
-expect(Label.from_json(expected_obj1).title).to eq expected_obj1['title']
-expect(Label.from_json(expected_obj1).color).to eq expected_obj1['color']
-expect(Label.from_json(expected_obj2).title).to eq expected_obj2['title']
-expect(Label.from_json(expected_obj2).color).to eq expected_obj2['color']
-end
-end
+      expect(Label.from_json(expected_obj1).title).to eq expected_obj1['title']
+      expect(Label.from_json(expected_obj1).color).to eq expected_obj1['color']
+      expect(Label.from_json(expected_obj2).title).to eq expected_obj2['title']
+      expect(Label.from_json(expected_obj2).color).to eq expected_obj2['color']
+    end
+  end
 end
