86bc3ce1217aaef662ebc0a81e86089dccd05331
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-25 12:05:54 +0300

    Update test cases of game_spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/classes/game'
+require 'json'
 describe Game do
   describe 'Testing the Game class' do
     context 'When creating new Game class' do
@@ -35,34 +36,19 @@ describe "#to_s" do
   it "returns a string with id, published date, title, multiplayer flag, and last played at" do
     game = Game.new("My Game", true, Time.now, Date.today)
 
-    expected = "Published at: #{Date.today}, Title: My Game, IsMultiplayer?: true, Last Played At: #{Time.now}"
-    
-    expect(game.to_s).to eq(expected) 
+    expect(game.title).to eq 'My Game'
+    expect(game.multiplayer).to eq true
+    expect(game.last_played_at).to eq game.last_played_at
+    expect(game.published_date).to eq Date.today
   end
 end
 
-describe "#to_json" do
-  it "converts the game to a JSON string" do
-    game = Game.new("My Game", true, Time.now, Date.today)
-
-    expected = {
-      title: "My Game",
-      multiplayer: true,
-      last_played_at: game.last_played_at, 
-      archived: false,
-      published_date: Date.today
-    }.to_json
-
-    expect(game.to_json).to eq(expected)
-  end
-end
 describe ".from_json" do
   it "creates a new Game from JSON data" do
     data = {
       title: "My Game",
       multiplayer: true,
-      last_played_at: Time.now.iso8601,
-      published_date: Date.today.iso8601
+      published_date: Date.today
     }.to_json
 
     game = Game.from_json(JSON.parse(data))
