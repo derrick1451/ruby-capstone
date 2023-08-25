0111d23234559055abdff08a47478b7ea166daf4
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-08-25 14:13:41 +0300

    Remove dublicated items table

--- sql/schema.sql
+++ sql/schema.sql
@@ -44,22 +44,6 @@ CREATE TABLE genres (
   PRIMARY KEY(id)
 );
 
-CREATE TABLE items (
-  id int GENERATED ALWAYS AS IDENTITY(MINVALUE 0 START WITH 0 CACHE 20) NOT NULL,
-  PRIMARY KEY (id),
-  genre varchar(30),
-  author varchar(30),
-  source varchar(30),
-  label varchar(30),
-  publish_date int,
-  archived boolean,
-  author_id int,
-  game_id int,
-  album_id int, 
-  genre_id int,
-  CONSTRAINT kf_albums FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE,
-  CONSTRAINT kf_genres FOREIGN KEY(genre_id) REFERENCES genres(id) ON DELETE CASCADE
-); 
 CREATE TABLE games (
   id int GENERATED ALWAYS AS IDENTITY(MINVALUE 0 START WITH 0 CACHE 20) NOT NULL,
   PRIMARY KEY (id),
@@ -75,19 +59,4 @@ CREATE TABLE authors (
   PRIMARY KEY (id),
   first_name varchar(30),
   last_name varchar(30)
-);
-
-CREATE TABLE items (
-  id int GENERATED ALWAYS AS IDENTITY(MINVALUE 0 START WITH 0 CACHE 20) NOT NULL,
-  PRIMARY KEY (id),
-  genre varchar(30),
-  author varchar(30),
-  source varchar(30),
-  label varchar(30),
-  publish_date date,
-  archived boolean,
-  author_id int,
-  game_id int,
-  CONSTRAINT kf_authors FOREIGN KEY(author_id) REFERENCES authors(id) ON DELETE CASCADE,
-  CONSTRAINT kf_games FOREIGN KEY(game_id) REFERENCES games(id) ON DELETE CASCADE
 );
